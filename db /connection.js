09a54a1826c0929bc5b2edbe00b3225e9395d52b
michellesambuu <michelle.sambuu@gmail.com>
2023-07-13 10:54:10 -0500

    added schema.sql

--- db /connection.js
+++ db /connection.js
@@ -2,9 +2,7 @@ const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
   host: "localhost",
-  // Your username
   user: "root",
-  // Your password
   password: "My091508",
   database: "employees_db"
 });
